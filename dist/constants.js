cfa541b7bf5247c3253b0235a08da568de10210b
sospedra <sospedra.r@gmail.com>
2017-08-12 18:27:52 +0200

    chore: Dont track dist files in the repo

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {};
-
-module.exports.TSIL_KEY = '__tsil__';
-module.exports.JOIN_PATH_KEY = '.';
-module.exports.ROOT_KEY = '~';
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var flatten = require('./flatten');
-var deflatten = require('./deflatten');
-
-module.exports = {
-  flatten: flatten,
-  deflatten: deflatten
-};
